0efdb08d419292de8f6ca383c492b876a77a793b
Fran Cebrián <122917827+FranCebrian@users.noreply.github.com>
2024-02-12 02:50:35 +0100

    Add files via upload

---
+++ docs/api/js/searchIndex.js
@@ -0,0 +1,14 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\obtenerNuevaImagen\u0028\u0029",
+            "name": "obtenerNuevaImagen",
+            "summary": "Funci\u00F3n\u0020para\u0020obtener\u0020una\u0020nueva\u0020imagen\u0020de\u0020Shiba\u0020Inu.",
+            "url": "namespaces/default.html#function_obtenerNuevaImagen"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
